fd367f56a23b55eb89dbec5331a22bbc304b2d06
Luiz Felipe Stangarlin <luizfelipestang@gmail.com>
2021-02-26 21:10:09 -0600

    prepare cmake

--- make.ps1
+++ make.ps1
@@ -1,22 +1,22 @@
 
 # pre
 Push-Location $PSScriptRoot
-$out = '../../sox-build/flac'
-$cmake = (New-Item -ItemType Directory $out -Force).FullName
-$bin = (New-Item -ItemType Directory $out/bin -Force).FullName
+$out = '../../sox-build/ogg'
+$build = (New-Item -ItemType Directory $out -Force).FullName
+$bin = (New-Item -ItemType Directory $out/../bin -Force).FullName
 
 $vswhere = "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe"
 $msbuild = & $vswhere -latest -requires Microsoft.Component.MSBuild -find "MSBuild\**\Bin\MSBuild.exe"
-$cmake_bootstrap = & $vswhere -latest -requires Microsoft.VisualStudio.Component.VC.CMake.Project -find "Common7\IDE\CommonExtensions\Microsoft\CMake\**\bin\cmake.exe"
+$cmake = & $vswhere -latest -requires Microsoft.VisualStudio.Component.VC.CMake.Project -find "Common7\IDE\CommonExtensions\Microsoft\CMake\**\bin\cmake.exe"
 
 # make
-& $cmake_bootstrap . -B $cmake
+& $cmake . -B $build
 
 # build
 
-# & $cmake_bootstrap --build $cmake --target install "-DCMAKE_BUILD_TYPE=Release" "-DCMAKE_INSTALL_PREFIX=$bin"
-& $msbuild $cmake/cmake.sln -maxCpuCount:16 -p:Configuration=Release -detailedSummary
-& $cmake_bootstrap --install $cmake --prefix $bin
+# & $cmake --build $build --target install "-DCMAKE_BUILD_TYPE=Release" "-DCMAKE_INSTALL_PREFIX=$bin"
+& $msbuild $build/libogg.sln -maxCpuCount:16 -p:Configuration=Release -detailedSummary
+& $cmake --install $build --prefix $bin
 
 # done
 Pop-Location
